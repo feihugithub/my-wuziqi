d6ea4c156f67489884ac7728fdadc5e7288978dd
feihu <feihu@feihu-PC>
2012-04-05 21:28:08 +0800

    更改了命名

---
+++ wuziqi/test/com/succez/wangzr/wuziqi/TestAi.java
@@ -0,0 +1,18 @@
+package com.succez.wangzr.wuziqi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.succez.wangzr.wuziqi.algorithm.Ai;
+
+public class TestAi {
+
+
+	@Test
+	public void testfind_min() {
+		
+		
+	}
+
+}
