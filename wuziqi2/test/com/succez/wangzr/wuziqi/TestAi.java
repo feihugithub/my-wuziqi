d845d05d5a78369495f48dc1bdd8528a39d4cab7
feihu <feihu@feihu-PC>
2012-04-18 17:43:49 +0800

    修改了相关的测试代码

--- wuziqi2/test/com/succez/wangzr/wuziqi/TestAi.java
+++ wuziqi2/test/com/succez/wangzr/wuziqi/TestAi.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import com.succez.wangzr.wuziqi.algorithm.Ai;
 import com.succez.wangzr.wuziqi.tools.ChessStyle;
 import com.succez.wangzr.wuziqi.tools.Constant;
+import com.succez.wangzr.wuziqi.ui.ChessPanel;
 
 /**
  * 测试Ai类的方法
@@ -18,23 +19,24 @@ public class TestAi extends TestCase {
 
 	@Test
 	public void testcompare() {
-		Ai ai = new Ai();
-		ai.table[0][0] = Constant.BLACKCHESS;
-		ai.table[1][1] = Constant.BLACKCHESS;
-		ai.table[0][1] = Constant.WHITECHESS;
-		ai.table[1][0] = Constant.BLACKCHESS;
+		ChessPanel chessPanel=new ChessPanel();
+		Ai ai=new Ai();
+		chessPanel.table[0][0] = Constant.BLACKCHESS;
+		chessPanel.table[1][1] = Constant.BLACKCHESS;
+		chessPanel.table[0][1] = Constant.WHITECHESS;
+		chessPanel.table[1][0] = Constant.BLACKCHESS;
 
-		assertEquals(Constant.CROSSBORDER, ai.compare(0, 0, -1, 0));
-		assertEquals(Constant.CROSSBORDER, ai.compare(0, 0, 0, -1));
-		assertEquals(Constant.CROSSBORDER, ai.compare(0, 0, -1, -1));
-		assertEquals(Constant.CROSSBORDER, ai.compare(0, 0, 1, -1));
-		assertEquals(Constant.CROSSBORDER, ai.compare(0, 0, -1, 1));
+		assertEquals(Constant.CROSSBORDER, ai.compare(0, 0, -1, 0,chessPanel.table));
+		assertEquals(Constant.CROSSBORDER, ai.compare(0, 0, 0, -1,chessPanel.table));
+		assertEquals(Constant.CROSSBORDER, ai.compare(0, 0, -1, -1,chessPanel.table));
+		assertEquals(Constant.CROSSBORDER, ai.compare(0, 0, 1, -1,chessPanel.table));
+		assertEquals(Constant.CROSSBORDER, ai.compare(0, 0, -1, 1,chessPanel.table));
 
-		assertEquals(Constant.DIFFERENT, ai.compare(0, 0, 3, 3));
-		assertEquals(Constant.DIFFERENT, ai.compare(0, 0, 0, 1));
+		assertEquals(Constant.DIFFERENT, ai.compare(0, 0, 3, 3,chessPanel.table));
+		assertEquals(Constant.DIFFERENT, ai.compare(0, 0, 0, 1,chessPanel.table));
 
-		assertEquals(Constant.SAME, ai.compare(0, 0, 1, 1));
-		assertEquals(Constant.SAME, ai.compare(0, 0, 1, 0));
+		assertEquals(Constant.SAME, ai.compare(0, 0, 1, 1,chessPanel.table));
+		assertEquals(Constant.SAME, ai.compare(0, 0, 1, 0,chessPanel.table));
 	}
 
 	@Test
@@ -89,137 +91,140 @@ public class TestAi extends TestCase {
 
 	@Test
 	public void testgetHorizChessStyle() {
+		ChessPanel chessPanel=new ChessPanel();
 		Ai ai = new Ai();
 		int test = -1;
-		ai.table[0][0] = Constant.BLACKCHESS;
-		test = ai.getHorizChessStyle(0, 0);
+		chessPanel.table[0][0] = Constant.BLACKCHESS;
+		test = ai.getHorizChessStyle(0, 0,chessPanel.table);
 		assertEquals(ChessStyle.SINGLE, test);
-		ai.table[1][0] = Constant.BLACKCHESS;
-		test = ai.getHorizChessStyle(0, 0);
+		chessPanel.table[1][0] = Constant.BLACKCHESS;
+		test = ai.getHorizChessStyle(0, 0,chessPanel.table);
 		assertEquals(ChessStyle.DEAD_2, test);
-		ai.table[2][0] = Constant.BLACKCHESS;
-		test = ai.getHorizChessStyle(0, 0);
+		chessPanel.table[2][0] = Constant.BLACKCHESS;
+		test = ai.getHorizChessStyle(0, 0,chessPanel.table);
 		assertEquals(ChessStyle.DEAD_3, test);
-		ai.table[3][0] = Constant.BLACKCHESS;
-		test = ai.getHorizChessStyle(0, 0);
+		chessPanel.table[3][0] = Constant.BLACKCHESS;
+		test = ai.getHorizChessStyle(0, 0,chessPanel.table);
 		assertEquals(ChessStyle.DEAD_4, test);
-		ai.table[4][0] = Constant.BLACKCHESS;
-		test = ai.getHorizChessStyle(0, 0);
+		chessPanel.table[4][0] = Constant.BLACKCHESS;
+		test = ai.getHorizChessStyle(0, 0,chessPanel.table);
 		assertEquals(ChessStyle.SUCCESS_5, test);
-		ai.table[0][0] = 0;
-		test = ai.getHorizChessStyle(1, 0);
+		chessPanel.table[0][0] = 0;
+		test = ai.getHorizChessStyle(1, 0,chessPanel.table);
 		assertEquals(ChessStyle.LIVE_4, test);
-		ai.table[4][0] = 0;
-		test = ai.getHorizChessStyle(1, 0);
+		chessPanel.table[4][0] = 0;
+		test = ai.getHorizChessStyle(1, 0,chessPanel.table);
 		assertEquals(ChessStyle.LIVE_3, test);
-		ai.table[3][0] = 0;
-		test = ai.getHorizChessStyle(1, 0);
+		chessPanel.table[3][0] = 0;
+		test = ai.getHorizChessStyle(1, 0,chessPanel.table);
 		assertEquals(ChessStyle.LIVE_2, test);
-		ai.table[2][0] = 0;
-		test = ai.getHorizChessStyle(1, 0);
+		chessPanel.table[2][0] = 0;
+		test = ai.getHorizChessStyle(1, 0,chessPanel.table);
 		assertEquals(ChessStyle.SINGLE, test);
 
-		ai.table[0][0] = Constant.WHITECHESS;
-		test = ai.getHorizChessStyle(1, 0);
+		chessPanel.table[0][0] = Constant.WHITECHESS;
+		test = ai.getHorizChessStyle(1, 0,chessPanel.table);
 		assertEquals(ChessStyle.SINGLE, test);
-		ai.table[2][0] = Constant.BLACKCHESS;
-		test = ai.getHorizChessStyle(1, 0);
+		chessPanel.table[2][0] = Constant.BLACKCHESS;
+		test = ai.getHorizChessStyle(1, 0,chessPanel.table);
 		assertEquals(ChessStyle.DEAD_2, test);
-		ai.table[3][0] = Constant.BLACKCHESS;
-		test = ai.getHorizChessStyle(1, 0);
+		chessPanel.table[3][0] = Constant.BLACKCHESS;
+		test = ai.getHorizChessStyle(1, 0,chessPanel.table);
 		assertEquals(ChessStyle.DEAD_3, test);
-		ai.table[4][0] = Constant.BLACKCHESS;
-		test = ai.getHorizChessStyle(1, 0);
+		chessPanel.table[4][0] = Constant.BLACKCHESS;
+		test = ai.getHorizChessStyle(1, 0,chessPanel.table);
 		assertEquals(ChessStyle.DEAD_4, test);
 	}
 
 	@Test
 	public void testpositionRate() {
+		ChessPanel chessPanel=new ChessPanel();
 		Ai ai = new Ai();
 		int test = -1;
-		ai.table[0][0] = Constant.BLACKCHESS;
-		test = ai.positionRate(0, 0);
+		chessPanel.table[0][0] = Constant.BLACKCHESS;
+		test = ai.positionRate(0, 0,chessPanel.table);
 		assertEquals(ChessStyle.SINGLE, test);
-		ai.table[1][0] = Constant.BLACKCHESS;
-		test = ai.positionRate(0, 0);
+		chessPanel.table[1][0] = Constant.BLACKCHESS;
+		test = ai.positionRate(0, 0,chessPanel.table);
 		assertEquals(ChessStyle.DEAD_2, test);
-		ai.table[2][0] = Constant.BLACKCHESS;
-		test = ai.positionRate(0, 0);
+		chessPanel.table[2][0] = Constant.BLACKCHESS;
+		test = ai.positionRate(0, 0,chessPanel.table);
 		assertEquals(ChessStyle.DEAD_3, test);
-		ai.table[3][0] = Constant.BLACKCHESS;
-		test = ai.positionRate(0, 0);
+		chessPanel.table[3][0] = Constant.BLACKCHESS;
+		test = ai.positionRate(0, 0,chessPanel.table);
 		assertEquals(ChessStyle.DEAD_4, test);
-		ai.table[4][0] = Constant.BLACKCHESS;
-		test = ai.positionRate(0, 0);
+		chessPanel.table[4][0] = Constant.BLACKCHESS;
+		test = ai.positionRate(0, 0,chessPanel.table);
 		assertEquals(ChessStyle.SUCCESS_5, test);
 
-		ai.table[0][0] = Constant.WHITECHESS;
-		test = ai.positionRate(4, 0);
+		chessPanel.table[0][0] = Constant.WHITECHESS;
+		test = ai.positionRate(4, 0,chessPanel.table);
 		assertEquals(ChessStyle.DEAD_4, test);
 
-		ai.table[2][1] = Constant.BLACKCHESS;
-		ai.table[3][2] = Constant.BLACKCHESS;
-		ai.table[4][3] = Constant.BLACKCHESS;
-		test = ai.positionRate(1, 0);
+		chessPanel.table[2][1] = Constant.BLACKCHESS;
+		chessPanel.table[3][2] = Constant.BLACKCHESS;
+		chessPanel.table[4][3] = Constant.BLACKCHESS;
+		test = ai.positionRate(1, 0,chessPanel.table);
 		assertEquals(ChessStyle.DOUBLE_DEAD_4 + 10, test);
 
-		ai.table[4][4] = Constant.BLACKCHESS;
-		ai.table[4][5] = Constant.BLACKCHESS;
-		test = ai.positionRate(4, 3);
+		chessPanel.table[4][4] = Constant.BLACKCHESS;
+		chessPanel.table[4][5] = Constant.BLACKCHESS;
+		test = ai.positionRate(4, 3,chessPanel.table);
 		assertEquals(ChessStyle.DEAD_4_LIVE_3 + 20, test);
 
-		ai.table[1][0] = 0;
-		test = ai.positionRate(4, 3);
+		chessPanel.table[1][0] = 0;
+		test = ai.positionRate(4, 3,chessPanel.table);
 		assertEquals(ChessStyle.DOUBLE_LIVE_3, test);
 
-		ai.table[1][0] = Constant.WHITECHESS;
-		test = ai.positionRate(4, 3);
+		chessPanel.table[1][0] = Constant.WHITECHESS;
+		test = ai.positionRate(4, 3,chessPanel.table);
 		assertEquals(ChessStyle.DEAD_3_LIVE_3, test);
 
-		ai.table[2][1] = 0;
-		ai.table[4][5] = 0;
-		test = ai.positionRate(4, 3);
+		chessPanel.table[2][1] = 0;
+		chessPanel.table[4][5] = 0;
+		test = ai.positionRate(4, 3,chessPanel.table);
 		assertEquals(ChessStyle.DOUBLE_LIVE_2, test);
 	}
 
 	@Test
 	public void testpanelRate() {
+		ChessPanel chessPanel=new ChessPanel();
 		Ai ai = new Ai();
-		ai.table[7][7] = Constant.BLACKCHESS;
-		ai.table[8][6] = Constant.BLACKCHESS;
-		ai.table[7][8] = Constant.BLACKCHESS;
-		ai.table[7][6] = Constant.BLACKCHESS;
-		ai.table[6][7] = Constant.BLACKCHESS;
-		ai.table[8][5] = Constant.BLACKCHESS;
-		ai.table[6][6] = Constant.BLACKCHESS;
-		ai.table[9][6] = Constant.BLACKCHESS;
-		ai.table[9][7] = Constant.BLACKCHESS;
-		ai.table[9][9] = Constant.BLACKCHESS;
-		ai.table[10][9] = Constant.BLACKCHESS;
+		chessPanel.table[7][7] = Constant.BLACKCHESS;
+		chessPanel.table[8][6] = Constant.BLACKCHESS;
+		chessPanel.table[7][8] = Constant.BLACKCHESS;
+		chessPanel.table[7][6] = Constant.BLACKCHESS;
+		chessPanel.table[6][7] = Constant.BLACKCHESS;
+		chessPanel.table[8][5] = Constant.BLACKCHESS;
+		chessPanel.table[6][6] = Constant.BLACKCHESS;
+		chessPanel.table[9][6] = Constant.BLACKCHESS;
+		chessPanel.table[9][7] = Constant.BLACKCHESS;
+		chessPanel.table[9][9] = Constant.BLACKCHESS;
+		chessPanel.table[10][9] = Constant.BLACKCHESS;
 
-		ai.table[8][8] = Constant.WHITECHESS;
-		ai.table[6][8] = Constant.WHITECHESS;
-		ai.table[7][9] = Constant.WHITECHESS;
-		ai.table[7][5] = Constant.WHITECHESS;
-		ai.table[6][9] = Constant.WHITECHESS;
-		//		ai.table[5][8]=Constant.WHITECHESS;
-		ai.table[8][9] = Constant.WHITECHESS;
-		ai.table[8][7] = Constant.WHITECHESS;
-		ai.table[10][6] = Constant.WHITECHESS;
-		ai.table[5][9] = Constant.WHITECHESS;
-		ai.table[4][9] = Constant.WHITECHESS;
+		chessPanel.table[8][8] = Constant.WHITECHESS;
+		chessPanel.table[6][8] = Constant.WHITECHESS;
+		chessPanel.table[7][9] = Constant.WHITECHESS;
+		chessPanel.table[7][5] = Constant.WHITECHESS;
+		chessPanel.table[6][9] = Constant.WHITECHESS;
+		//		chessPanel.table[5][8]=Constant.WHITECHESS;
+		chessPanel.table[8][9] = Constant.WHITECHESS;
+		chessPanel.table[8][7] = Constant.WHITECHESS;
+		chessPanel.table[10][6] = Constant.WHITECHESS;
+		chessPanel.table[5][9] = Constant.WHITECHESS;
+		chessPanel.table[4][9] = Constant.WHITECHESS;
 
-		int test = ai.panelRate(Constant.BLACKCHESS);
+		int test = ai.panelRate(Constant.BLACKCHESS,chessPanel.table);
 		assertEquals(ChessStyle.DEAD_4_LIVE_3 + 20, test);
-		test = ai.panelRate(Constant.WHITECHESS);
+		test = ai.panelRate(Constant.WHITECHESS,chessPanel.table);
 		assertEquals(-ChessStyle.SUCCESS_5, test);
-		test = ai.maxRate();
+		test = ai.maxRate(chessPanel.table);
 		assertEquals(Constant.LOSE, test);
-		ai.table[5][6] = Constant.BLACKCHESS;
-		test = ai.maxRate();
+		chessPanel.table[5][6] = Constant.BLACKCHESS;
+		test = ai.maxRate(chessPanel.table);
 		assertEquals(Constant.LOSE, test);
-		ai.table[4][9] = 0;
-		test = ai.maxRate();
+		chessPanel.table[4][9] = 0;
+		test = ai.maxRate(chessPanel.table);
 		assertEquals(Constant.WIN, test);
 	}
 
--- wuziqi2/test/com/succez/wangzr/wuziqi/TestChessMethod.java
+++ wuziqi2/test/com/succez/wangzr/wuziqi/TestChessMethod.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import com.succez.wangzr.wuziqi.algorithm.ChessMethod;
 import com.succez.wangzr.wuziqi.tools.Constant;
 import com.succez.wangzr.wuziqi.tools.Point;
+import com.succez.wangzr.wuziqi.ui.ChessPanel;
 
 /**
  * ChessMethod测试类
@@ -14,98 +15,99 @@ import com.succez.wangzr.wuziqi.tools.Point;
  * @createdate 2012-4-9
  */
 public class TestChessMethod {
-	ChessMethod method = new ChessMethod();
+	ChessPanel chessPanel = new ChessPanel();
+	ChessMethod method=new ChessMethod();
 
 	@Test
 	public void testreset() {
-		method.ai.table[0][0] = Constant.BLACKCHESS;
-		method.ai.table[1][1] = Constant.BLACKCHESS;
-		method.ai.table[2][2] = Constant.BLACKCHESS;
-		assertEquals(Constant.BLACKCHESS, method.ai.table[0][0]);
-		assertEquals(Constant.BLACKCHESS, method.ai.table[1][1]);
-		assertEquals(Constant.BLACKCHESS, method.ai.table[2][2]);
-		method.resetChessPanel();
-		assertEquals(0, method.ai.table[0][0]);
-		assertEquals(0, method.ai.table[1][1]);
-		assertEquals(0, method.ai.table[2][2]);
+		chessPanel.table[0][0] = Constant.BLACKCHESS;
+		chessPanel.table[1][1] = Constant.BLACKCHESS;
+		chessPanel.table[2][2] = Constant.BLACKCHESS;
+		assertEquals(Constant.BLACKCHESS, chessPanel.table[0][0]);
+		assertEquals(Constant.BLACKCHESS, chessPanel.table[1][1]);
+		assertEquals(Constant.BLACKCHESS, chessPanel.table[2][2]);
+		method.resetChessPanel(chessPanel.table);
+		assertEquals(0, chessPanel.table[0][0]);
+		assertEquals(0, chessPanel.table[1][1]);
+		assertEquals(0, chessPanel.table[2][2]);
 	}
 
 	@Test
 	public void testisWin() {
-		method.ai.table[7][5] = Constant.BLACKCHESS;
-		method.ai.table[7][6] = Constant.BLACKCHESS;
-		method.ai.table[7][7] = Constant.BLACKCHESS;
-		method.ai.table[7][8] = Constant.BLACKCHESS;
-		method.ai.table[7][9] = Constant.BLACKCHESS;
-		boolean b = method.isWin(7, 7);
+		chessPanel.table[7][5] = Constant.BLACKCHESS;
+		chessPanel.table[7][6] = Constant.BLACKCHESS;
+		chessPanel.table[7][7] = Constant.BLACKCHESS;
+		chessPanel.table[7][8] = Constant.BLACKCHESS;
+		chessPanel.table[7][9] = Constant.BLACKCHESS;
+		boolean b = method.isWin(7, 7,chessPanel.table);
 		assertEquals(true, b);
-		b = method.isWin(7, 5);
+		b = method.isWin(7, 5,chessPanel.table);
 		assertEquals(true, b);
-		b = method.isWin(7, 9);
+		b = method.isWin(7, 9,chessPanel.table);
 		assertEquals(true, b);
-		method.resetChessPanel();
+		method.resetChessPanel(chessPanel.table);
 
-		method.ai.table[5][7] = Constant.BLACKCHESS;
-		method.ai.table[6][7] = Constant.BLACKCHESS;
-		method.ai.table[7][7] = Constant.BLACKCHESS;
-		method.ai.table[8][7] = Constant.BLACKCHESS;
-		method.ai.table[9][7] = Constant.BLACKCHESS;
-		b = method.isWin(5, 7);
+		chessPanel.table[5][7] = Constant.BLACKCHESS;
+		chessPanel.table[6][7] = Constant.BLACKCHESS;
+		chessPanel.table[7][7] = Constant.BLACKCHESS;
+		chessPanel.table[8][7] = Constant.BLACKCHESS;
+		chessPanel.table[9][7] = Constant.BLACKCHESS;
+		b = method.isWin(5, 7,chessPanel.table);
 		assertEquals(true, b);
-		b = method.isWin(7, 7);
+		b = method.isWin(7, 7,chessPanel.table);
 		assertEquals(true, b);
-		b = method.isWin(9, 7);
+		b = method.isWin(9, 7,chessPanel.table);
 		assertEquals(true, b);
-		method.resetChessPanel();
+		method.resetChessPanel(chessPanel.table);
 
-		method.ai.table[5][5] = Constant.BLACKCHESS;
-		method.ai.table[6][6] = Constant.BLACKCHESS;
-		method.ai.table[7][7] = Constant.BLACKCHESS;
-		method.ai.table[8][8] = Constant.BLACKCHESS;
-		method.ai.table[9][9] = Constant.BLACKCHESS;
-		b = method.isWin(5, 5);
+		chessPanel.table[5][5] = Constant.BLACKCHESS;
+		chessPanel.table[6][6] = Constant.BLACKCHESS;
+		chessPanel.table[7][7] = Constant.BLACKCHESS;
+		chessPanel.table[8][8] = Constant.BLACKCHESS;
+		chessPanel.table[9][9] = Constant.BLACKCHESS;
+		b = method.isWin(5, 5,chessPanel.table);
 		assertEquals(true, b);
-		b = method.isWin(7, 7);
+		b = method.isWin(7, 7,chessPanel.table);
 		assertEquals(true, b);
-		b = method.isWin(9, 9);
+		b = method.isWin(9, 9,chessPanel.table);
 		assertEquals(true, b);
 
-		method.ai.table[9][9] = Constant.WHITECHESS;
-		b = method.isWin(7, 7);
+		chessPanel.table[9][9] = Constant.WHITECHESS;
+		b = method.isWin(7, 7,chessPanel.table);
 		assertEquals(false, b);
-		method.ai.table[9][9] = 0;
-		b = method.isWin(7, 7);
+		chessPanel.table[9][9] = 0;
+		b = method.isWin(7, 7,chessPanel.table);
 		assertEquals(false, b);
-		method.resetChessPanel();
+		method.resetChessPanel(chessPanel.table);
 
-		method.ai.table[5][9] = Constant.BLACKCHESS;
-		method.ai.table[6][8] = Constant.BLACKCHESS;
-		method.ai.table[7][7] = Constant.BLACKCHESS;
-		method.ai.table[8][6] = Constant.BLACKCHESS;
-		method.ai.table[9][5] = Constant.BLACKCHESS;
-		b = method.isWin(5, 9);
+		chessPanel.table[5][9] = Constant.BLACKCHESS;
+		chessPanel.table[6][8] = Constant.BLACKCHESS;
+		chessPanel.table[7][7] = Constant.BLACKCHESS;
+		chessPanel.table[8][6] = Constant.BLACKCHESS;
+		chessPanel.table[9][5] = Constant.BLACKCHESS;
+		b = method.isWin(5, 9,chessPanel.table);
 		assertEquals(true, b);
-		b = method.isWin(7, 7);
+		b = method.isWin(7, 7,chessPanel.table);
 		assertEquals(true, b);
-		b = method.isWin(9, 5);
+		b = method.isWin(9, 5,chessPanel.table);
 		assertEquals(true, b);
-		method.resetChessPanel();
+		method.resetChessPanel(chessPanel.table);
 	}
 
 	@Test
 	public void testcompare() {
-		method.ai.table[0][0] = Constant.BLACKCHESS;
-		method.ai.table[0][1] = Constant.BLACKCHESS;
-		method.ai.table[1][0] = Constant.WHITECHESS;
-		boolean b = method.compare(0, 0, 0, -1);
+		chessPanel.table[0][0] = Constant.BLACKCHESS;
+		chessPanel.table[0][1] = Constant.BLACKCHESS;
+		chessPanel.table[1][0] = Constant.WHITECHESS;
+		boolean b = method.compare(0, 0, 0, -1,chessPanel.table);
 		assertEquals(false, b);
-		b = method.compare(0, 0, -1, 0);
+		b = method.compare(0, 0, -1, 0,chessPanel.table);
 		assertEquals(false, b);
-		b = method.compare(0, 0, 1, 0);
+		b = method.compare(0, 0, 1, 0,chessPanel.table);
 		assertEquals(false, b);
-		b = method.compare(0, 0, 0, 1);
+		b = method.compare(0, 0, 0, 1,chessPanel.table);
 		assertEquals(true, b);
-		b = method.compare(0, 0, 1, 1);
+		b = method.compare(0, 0, 1, 1,chessPanel.table);
 		assertEquals(false, b);
 	}
 
